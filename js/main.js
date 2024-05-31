b36ef6fc997999ab9bb64fcead154380aae6a5af
chika-bac <cocopanda.it@gmail.com>
2024-06-01 00:08:37 +0900

    インタラクション、ピクパなど最終修正

--- js/main.js
+++ js/main.js
@@ -12,52 +12,13 @@ if (menuIcon) {
   });
 }
 
-// /*
-// Intersection Observer: js
-// ================================================ */
-// const io = new IntersectionObserver((entries) => {
-//   entries.forEach((entry, observer) => {
-//     if (entry.isIntersecting) {
-//       // 要素が表示領域に入った場合にクラスを付与
-//       entry.target.classList.add("is-in-view");
-//     }
-//     // 表示領域から外れた場合
-//     // else {
-//     //   entry.target.classList.remove("is-in-view");
-//     // }
-//   });
-// });
-
-// const inViewItems = document.querySelectorAll(".js-in-view");
-// inViewItems.forEach((inViewItem) => {
-//   io.observe(inViewItem);
-// });
-
-// // mapを使った書き方
-// // const inViewItems = [...document.querySelectorAll(".js-in-view")];
-// // inViewItems.map((inViewItem) => io.observe(inViewItem));
-
-// /*
-// スムーススクロール：jQuery
-// ================================================ */
-// // href属性が#で始まるaタグをクリックした場合
-// $('a[href^="#"]').on("click", function (e) {
-//   // クリックした要素のhref属性を取得
-//   const id = $(this).attr("href");
-//   // href属性が#のみの場合はトップ（html）、それ以外はid属性のリンク先
-//   const target = $(id == "#" ? "html" : id);
-//   const position = $(target).offset().top;
-//   $("html, body").animate(
-//     {
-//       scrollTop: position,
-//     },
-//     1000,
-//     "swing" // swing or linear
-//   );
-// });
-
-// // ドロワーメニュー内のリンククリック時にドロワーを閉じる
-// $('#js-drawer-content a[href^="#"]').on("click", function (e) {
-//   $("#js-drawer-icon").removeClass("is-checked");
-//   $("#js-drawer-content").removeClass("is-checked");
-// });
+/*
+スムーススクロール：js
+================================================ */
+// ドロワーメニュー内のリンククリック時にドロワーを閉じる
+document.querySelectorAll('#js-drawer-panel a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", (e) => {
+    menuIcon.classList.remove("is-checked");
+    menuPanel.classList.remove("is-checked");
+  });
+});
