e9c4a0ed239c4e5408581847eb35125e89ef5c47
chika-bac <cocopanda.it@gmail.com>
2024-07-05 09:28:57 +0900

    フォームバリデーションとviewport固定

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-// /*
-// スライドメニュー: js
-// ================================================ */
+/*
+360pxでviewport固定
+================================================ */
+!(function () {
+  const viewport = document.querySelector('meta[name="viewport"]');
+  function switchViewport() {
+    const value = window.outerWidth > 360 ? "width=device-width,initial-scale=1" : "width=360";
+    if (viewport.getAttribute("content") !== value) {
+      viewport.setAttribute("content", value);
+    }
+  }
+  addEventListener("resize", switchViewport, false);
+  switchViewport();
+})();
+
+/*
+スライドメニュー: js
+================================================ */
 const menuIcon = document.querySelector("#js-drawer-icon");
 const menuPanel = document.querySelector("#js-drawer-panel");
 
@@ -9,6 +24,7 @@ if (menuIcon) {
     e.preventDefault();
     menuIcon.classList.toggle("is-checked");
     menuPanel.classList.toggle("is-checked");
+    document.body.classList.toggle("isScrollAllowed");
   });
 }
 
@@ -20,5 +36,16 @@ document.querySelectorAll('#js-drawer-panel a[href^="#"]').forEach((link) => {
   link.addEventListener("click", (e) => {
     menuIcon.classList.remove("is-checked");
     menuPanel.classList.remove("is-checked");
+    document.body.classList.remove("isScrollAllowed");
   });
+});
+
+/*
+フォームバリデーション
+================================================ */
+const form = document.querySelector("#js-form");
+form.addEventListener("submit", (e) => {
+  if (form.checkValidity()) {
+    alert("送信が完了しました");
+  }
 });
